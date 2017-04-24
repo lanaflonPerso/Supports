e51b071e100b74d0b9c97ff3aeeb84965c35b560
Nicolas Rousset <nrousset@gmail.com>
2017-04-24 09:57:55 +0200

    Workspace

---
+++ Prairie_2017_04_24_lundi/Formation_simplon_2017_04_24/src/AgencePoleEmploi.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+
+public class AgencePoleEmploi {
+	private List<Candidat>   liste_candidats;
+	private List<Conseiller> liste_conseillers;
+	
+	
+}
---
+++ Prairie_2017_04_24_lundi/Formation_simplon_2017_04_24/src/Creneau.java
@@ -0,0 +1,24 @@
+import java.util.Date;
+
+
+public class Creneau {
+	private Date debut;
+	private Date fin;
+	
+	
+	
+	public Date getDebut() {
+		return debut;
+	}
+	public void setDebut(Date debut) {
+		this.debut = debut;
+	}
+	public Date getFin() {
+		return fin;
+	}
+	public void setFin(Date fin) {
+		this.fin = fin;
+	}
+	
+	
+}
