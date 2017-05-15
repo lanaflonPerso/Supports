f06e9c2bc905d0a3bc2c34ddf891d5e0ccec946f
Nicolas Rousset <nrousset@gmail.com>
2017-05-15 07:09:21 +0200

    Adding day 1 support

---
+++ Session_01_2017_05_14_06_02/Vue_general_formation_developpeur_java_web.mm
@@ -0,0 +1,196 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Formation - d&#xe9;veloppeur Java web &quot;full stack&quot;" LOCALIZED_STYLE_REF="default" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1494768493055" MIN_WIDTH="1"><hook NAME="MapStyle">
+    <properties show_icon_for_attributes="true" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="10"/>
+<node TEXT="Programme de la formation" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="right" ID="ID_259657335" CREATED="1494766374316" MODIFIED="1494768472653" MAX_WIDTH="250" MIN_WIDTH="1">
+<edge COLOR="#7c007c"/>
+<node TEXT="D&#xe9;veloppement" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_1439969353" CREATED="1494764351010" MODIFIED="1494768472655" MAX_WIDTH="250" MIN_WIDTH="1">
+<node TEXT="Back-end" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_1452077793" CREATED="1494764312267" MODIFIED="1494773918799" MAX_WIDTH="250" MIN_WIDTH="1"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <i>Le back-end est la partie qui g&#232;re la requ&#234;te re&#231;ue et g&#233;n&#232;re la r&#233;ponse renvoy&#233;e (une page HTML dans le cas d'un site public) &#224; partir de la logique interne et de la base de donn&#233;e.</i>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="Base de donn&#xe9;e" ID="ID_741630851" CREATED="1494764403056" MODIFIED="1494768472657" MAX_WIDTH="250" MIN_WIDTH="1">
+<node TEXT="Conceptions, norme" ID="ID_1048095989" CREATED="1494765414823" MODIFIED="1494773889083" MAX_WIDTH="250" MIN_WIDTH="1">
+<font BOLD="true"/>
+<node TEXT="Il existe un certain nombre de normes en conception de base de donn&#xe9;e qui ont pour objectif notamment :&#xa;- de faciliter les traitements automatis&#xe9;s&#xa;- d&apos;&#xe9;viter la redondance de donn&#xe9;e&#xa;- d&apos;assurer la coh&#xe9;rence des donn&#xe9;es" ID="ID_1976523799" CREATED="1494770179276" MODIFIED="1494770331119" MAX_WIDTH="250"/>
+</node>
+<node TEXT="SQL" ID="ID_1673057759" CREATED="1494765422327" MODIFIED="1494773889084" MAX_WIDTH="250" MIN_WIDTH="1">
+<font BOLD="true"/>
+<node TEXT="Structured Query Langage : le langage usuel de requ&#xea;te en base de donn&#xe9;e" ID="ID_1683465387" CREATED="1494769528181" MODIFIED="1494769553533" MAX_WIDTH="250"/>
+</node>
+<node TEXT="Diff&#xe9;rents SGBDR" ID="ID_1621867719" CREATED="1494765427289" MODIFIED="1494773889085" MAX_WIDTH="250" MIN_WIDTH="1">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="Java" ID="ID_4373613" CREATED="1494764382860" MODIFIED="1494768472658" MAX_WIDTH="250" MIN_WIDTH="1">
+<node TEXT="API, structure, concepts" ID="ID_207557691" CREATED="1494765483033" MODIFIED="1494773889085" MAX_WIDTH="250" MIN_WIDTH="1">
+<font BOLD="true"/>
+</node>
+<node TEXT="Programmation Orient&#xe9;e Objet" ID="ID_1803347444" CREATED="1494765509734" MODIFIED="1494773889086" MAX_WIDTH="250" MIN_WIDTH="1">
+<font BOLD="true"/>
+<node TEXT="Le concept n&apos;est pas limit&#xe9;e &#xe0; Java, mais particuli&#xe8;rement pr&#xe9;sent dans le langage" ID="ID_1926485186" CREATED="1494769680775" MODIFIED="1494769704105" MAX_WIDTH="250"/>
+</node>
+<node TEXT="JPA / Hibernate" ID="ID_1173800656" CREATED="1494765462230" MODIFIED="1494773889086" MAX_WIDTH="250" MIN_WIDTH="1">
+<font BOLD="true"/>
+<node TEXT="Le standard d&apos;acc&#xe8;s aux donn&#xe9;es en java et son impl&#xe9;mentation la plus utilis&#xe9;e" ID="ID_1848976018" CREATED="1494769711690" MODIFIED="1494769737526" MAX_WIDTH="250"/>
+</node>
+</node>
+</node>
+<node TEXT="Front-end" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_1074989262" CREATED="1494764333124" MODIFIED="1494770027568" MAX_WIDTH="250" MIN_WIDTH="1"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <i>Le front-end est la partie vue par l'utilisateur.</i>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="HTML / template" ID="ID_1517987643" CREATED="1494764455157" MODIFIED="1494773889086" MAX_WIDTH="250" MIN_WIDTH="1">
+<font BOLD="true"/>
+<node TEXT="Le langage de base des pages web, qui sert de support &#xe0; tous les &#xe9;l&#xe9;ments de la page (image, script, css, etc ...)&#xa;Il est repr&#xe9;sent&#xe9; dans la base de donn&#xe9;e sous forme de template g&#xe9;n&#xe9;riques qui sont remplis avec les informations sp&#xe9;cifiques." ID="ID_1379733335" CREATED="1494772528229" MODIFIED="1494772606161"/>
+</node>
+<node TEXT="Css" ID="ID_284436045" CREATED="1494764465098" MODIFIED="1494773889087" MAX_WIDTH="250" MIN_WIDTH="1">
+<font BOLD="true"/>
+<node TEXT="Cascading Style Sheet : les feuilles de style, qui contiennent les informations de mise en forme." ID="ID_1401898301" CREATED="1494772609704" MODIFIED="1494772707053" MAX_WIDTH="250"/>
+</node>
+<node TEXT="Javascript" ID="ID_325383233" CREATED="1494764469561" MODIFIED="1494773889087" MAX_WIDTH="250" MIN_WIDTH="1"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <i>Le presque seul langage de programmation dynamique ex&#233;cutable dans le navigateur, il permet de modifier dynamiquement et rapidement le code HTML et donc l'affichage de la page.</i>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<font BOLD="true"/>
+<node TEXT="AngularJS" ID="ID_971203296" CREATED="1494764488291" MODIFIED="1494768472662" MAX_WIDTH="250" MIN_WIDTH="1">
+<node TEXT="Un framework (ensemble de librairie et de fonction) de d&#xe9;veloppement en javascript" ID="ID_206137217" CREATED="1494772715461" MODIFIED="1494772814354" MAX_WIDTH="250"/>
+</node>
+</node>
+</node>
+<node TEXT="Transverse" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_930237217" CREATED="1494765540289" MODIFIED="1494768472662" MAX_WIDTH="250" MIN_WIDTH="1">
+<node TEXT="Tests Unitaires" ID="ID_699062049" CREATED="1494765545976" MODIFIED="1494773889087" MAX_WIDTH="250" MIN_WIDTH="1">
+<font BOLD="true"/>
+</node>
+<node TEXT="Algorithmie" ID="ID_132442891" CREATED="1494765553023" MODIFIED="1494773889087" MAX_WIDTH="250" MIN_WIDTH="1">
+<font BOLD="true"/>
+<node TEXT="Principes de base d&apos;algorithmie, notamment la question des acc&#xe8;s aux donn&#xe9;es, centrales en informatique (li&#xe9;e aux structures de donn&#xe9;e java, aux index en base de donn&#xe9;e, etc ...)" ID="ID_1265704286" CREATED="1494772823797" MODIFIED="1494772872598"/>
+</node>
+</node>
+</node>
+<node TEXT="Outils" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_1485407590" CREATED="1494765230517" MODIFIED="1494768472663" MAX_WIDTH="250" MIN_WIDTH="1">
+<node TEXT="Outils de gestion de projet" ID="ID_527952626" CREATED="1494765109850" MODIFIED="1494768472664" MAX_WIDTH="250" MIN_WIDTH="1">
+<node TEXT="Maven" ID="ID_705999750" CREATED="1494765146131" MODIFIED="1494773889087" MAX_WIDTH="250" MIN_WIDTH="1">
+<font BOLD="true"/>
+<node TEXT="Outil de gestion de d&#xe9;pendance : on indique dans un fichier les d&#xe9;pendances du projet et maven g&#xe8;re la configuration avec les diff&#xe9;rents programmes : apache, eclipse" ID="ID_1608122416" CREATED="1494766964681" MODIFIED="1494768472666" MAX_WIDTH="250" MIN_WIDTH="1">
+<hook NAME="AlwaysUnfoldedNode"/>
+</node>
+</node>
+<node TEXT="Git" ID="ID_766791075" CREATED="1494765150830" MODIFIED="1494773889088" MAX_WIDTH="250" MIN_WIDTH="1">
+<font BOLD="true"/>
+<node TEXT="Outil de gestion de configuration : on y sauvegarde les fichiers (push) afin de les partager et de pouvoir retrouver les diff&#xe9;rentes versions (et modification sur chacun)" ID="ID_131761420" CREATED="1494768441851" MODIFIED="1494768659317" MAX_WIDTH="250" MIN_WIDTH="1"/>
+</node>
+<node TEXT="Outils de d&#xe9;bogage" ID="ID_801292647" CREATED="1494765155150" MODIFIED="1494773889088" MAX_WIDTH="250" MIN_WIDTH="1">
+<font BOLD="true"/>
+</node>
+<node TEXT="Jenkins" ID="ID_1206459567" CREATED="1494766244165" MODIFIED="1494773889088" MAX_WIDTH="250" MIN_WIDTH="1">
+<font BOLD="true"/>
+<node TEXT="Outil d&apos;int&#xe9;gration continue : a chaque modification du code, on lance automatiquement les tests et/ou l&apos;int&#xe9;gration, afin de d&#xe9;tecter les bugs au fur et &#xe0; mesure" ID="ID_1333508970" CREATED="1494769251317" MODIFIED="1494769309409" MAX_WIDTH="250"/>
+</node>
+</node>
+<node TEXT="Outil de conception" ID="ID_1008134873" CREATED="1494765245222" MODIFIED="1494768472671" MAX_WIDTH="250" MIN_WIDTH="1">
+<node TEXT="UML" ID="ID_1720548664" CREATED="1494765274472" MODIFIED="1494773889088" MAX_WIDTH="250" MIN_WIDTH="1">
+<font BOLD="true"/>
+<node TEXT="Universal Markup Langage : comme son nom l&apos;indique on peut l&apos;utiliser pour repr&#xe9;senter beaucoup de diagramme diff&#xe9;rents, mais en ici principalement les mod&#xe8;les de donn&#xe9;e et les diagramme de classe" ID="ID_1373116660" CREATED="1494768707177" MODIFIED="1494768859285" MAX_WIDTH="250"/>
+</node>
+</node>
+</node>
+<node TEXT="Architecture" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_245994790" CREATED="1494766384174" MODIFIED="1494768472672" MAX_WIDTH="250" MIN_WIDTH="1">
+<node TEXT="Architecture MVC" ID="ID_1205096369" CREATED="1494766389394" MODIFIED="1494773889088" MAX_WIDTH="250" MIN_WIDTH="1">
+<font BOLD="true"/>
+<node TEXT="Model View Controller : il s&apos;agit de l&apos;architecture usuelle c&#xf4;t&#xe9; serveur, o&#xf9; l&apos;on s&#xe9;pare les 3 aspects ; Mod&#xe8;le de donn&#xe9;e, View (affichage) et Contr&#xf4;lleur (gestion des requ&#xea;tes)" ID="ID_1267262875" CREATED="1494769357538" MODIFIED="1494769424566"/>
+</node>
+<node TEXT="Architecture N-tier" ID="ID_876840218" CREATED="1494766406403" MODIFIED="1494773889088" MAX_WIDTH="250" MIN_WIDTH="1">
+<font BOLD="true"/>
+</node>
+<node TEXT="Architecture REST" ID="ID_458307993" CREATED="1494766577143" MODIFIED="1494773889089" MAX_WIDTH="250" MIN_WIDTH="1">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="Environnement WEB" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_1617182486" CREATED="1494766424753" MODIFIED="1494768472674" MAX_WIDTH="250" MIN_WIDTH="1">
+<node TEXT="Protocole HTTP / FTP / SSL" ID="ID_643820013" CREATED="1494766583500" MODIFIED="1494773889089" MAX_WIDTH="250" MIN_WIDTH="1">
+<font BOLD="true"/>
+</node>
+</node>
+</node>
+</node>
+</map>
