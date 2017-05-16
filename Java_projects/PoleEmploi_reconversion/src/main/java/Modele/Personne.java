3b02433158115695a28ef77ac51ca0de3048d9b8
Nicolas Rousset <nrousset@gmail.com>
2017-05-16 09:50:37 +0200

    WiP

--- Java_projects/PoleEmploi_reconversion/src/main/java/Modele/Personne.java
+++ Java_projects/PoleEmploi_reconversion/src/main/java/Modele/Personne.java
@@ -26,14 +26,7 @@ public class Personne {
 	
 	public static Personne readFromFileLine(String fileLine)
 	{
-		String[] splitString = fileLine.split(" ");
-		
-		return new Personne(
-				Integer.parseInt(splitString[0]),
-				Long.parseLong(splitString[3]),
-				splitString[2],
-				splitString[1]
-				);
+		return null;
 	}
 	
 	public static List<Personne> readAllFromFiles(String fileName) throws IOException
@@ -50,58 +43,5 @@ public class Personne {
 		return result;
 	}
 	
-	public Personne()
-	{
-		
-	}
-	
-	public Personne(int id, long numeroDeSecuriteSociale, String nom, String prenom )
-	{
-		this.id = id;
-		this.numeroDeSecuriteSociale = numeroDeSecuriteSociale;
-		this.nom = nom;
-		this.prenom = prenom;
-	}
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public long getNumeroDeSecuriteSociale() {
-		return numeroDeSecuriteSociale;
-	}
-	public void setNumeroDeSecuriteSociale(long numero_de_securite_sociale) {
-		this.numeroDeSecuriteSociale = numeroDeSecuriteSociale;
-	}
-	public String getNom() {
-		return nom;
-	}
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-	public String getPrenom() {
-		return prenom;
-	}
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
-	
-	public boolean estMasculin()
-	{
-		return ("" + this.getNumeroDeSecuriteSociale()).substring(0, 1).equals("1");
-	}
-	
-	public int getNumeroDepartement()
-	{
-		return Integer.parseInt(("" + this.getNumeroDeSecuriteSociale()).substring(1,3));
-	}
-	
-	public String toString()
-	{
-		return String.format("%s %s : %s", this.getPrenom(), this.getNom(),
-				             this.getNumeroDeSecuriteSociale());
-	}
 	
 }
