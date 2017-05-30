1d5eeee2b3290cdd159958ff75a965991c26d12b
Nicolas Rousset <nrousset@gmail.com>
2017-05-30 14:18:06 +0200

    Ajout exemple villes

---
+++ Session_01_2017_05_14_06_02/Journee_10_2017_05_30/MainTest.java
@@ -0,0 +1,16 @@
+public class MainTest
+{
+    public static void main(String[] args)
+    {
+        Ville paris      = new Ville("Paris"     , 48.856614, 2.3522219);
+        Ville brest      = new Ville("Brest"     , 48.4084597,-4.5347058);
+        Ville lille      = new Ville("Lille"     , 50.6311167,3.0120553);
+        Ville strasbourg = new Ville("Strasbourg", 48.5692058,7.6918829);
+        Ville nice       = new Ville("Nice"      , 43.7031654,7.1827232);
+        Ville marseille  = new Ville("Marseille" , 43.2804941,5.3349277);
+        Ville biarritz   = new Ville("Biarritz"  , 43.4710109,-1.573345);
+        Ville rioDeJaneiro   = new Ville("Rio de Janeiro",  
+                                                 -22.9108555,-43.5887609);
+    }
+}
+
